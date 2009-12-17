a8d81883ca89b8cea7ab7874589e218e0ceb0bb7
Jarrett Meyer <jarrettmeyer@gmail.com>
2009-12-17 13:50:13 -0500

    First commit.

---
+++ simple-format-preview.js
@@ -0,0 +1,24 @@
+var SimpleFormatPreview = Class.create();
+
+SimpleFormatPreview.prototype = {
+    initialize: function(source, preview) {
+        this.source = source;
+        this.preview = preview;
+        this.onKeyUp(); // to initialize the preview area        
+        $(this.source).observe('keyup', this.onKeyUp.bind(this));
+    },
+    onKeyUp: function() {
+        var content = $F(this.source).escapeHTML();
+        content = content.replace(/^\s+|\s+$/, '');
+        if (content == '') {
+            $(this.preview).hide();
+            return;
+        } else {
+            $(this.preview).show();
+        }
+        content = content.replace(/\n{3,}/g, '\n\n');
+        content = content.replace(/\n\n/g, '</p><p>').replace(/\n/g, '<br/>');
+        content = '<p>' + content + '</p>';
+        $(this.preview).innerHTML = content;
+    }
+};
